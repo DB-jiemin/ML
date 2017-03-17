9fa08fe834053603988672034f1668c02f678145
jiemin_zhang <jiemin_zhang@talkmate.com>
2017-03-17 10:27:41 +0800

    commit mRMR

--- LogisticRegression/lr.py
+++ LogisticRegression/lr.py
@@ -10,29 +10,20 @@ class LogisticRegression():
     def sigmoid(self, x):
         return 1.0 / (1 + np.exp(-x))
 
+    # 梯度下降
     def _gradDescent(self, featData, labelData, alpha, maxCycles):
         dataMat = np.mat(featData)
 	labelMat = np.mat(labelData).transpose()
 	m, n = np.shape(dataMat)
 	weigh = np.ones((n, 1))
 	for i in range(maxCycles):
-	    hx = self._sigmoid(dataMat * weigh)
-	    error = labelMat - hx
-	    weigh = weigh + alpha * dataMat.transpose() * error
+	    hx = self.sigmoid(dataMat * weigh)
+	    error = labelMat - hx # 计算惨差
+	    weigh = weigh + alpha * dataMat.transpose() * error # 更系权重
 	return weigh
     
-    def stocGradDescent(self, featData, labelData, alpha, maxCycles):
-        dataMat = np.mat(featData)
-	labelMat = np.mat(labelData)
-        m, n = np.shape(dataMat)
-	weights = np.ones((n, 1))
-	for i in range(maxCycles):
-	    h = self.sigmoid(dataMat * weights)
-	    error = h - labelMat
-	    weights = weights + alpha * error * dataMat
-	return weights
-    
-    def stocGradDescent_1(self, featData, labelData, maxCycles):
+    # 随机梯度下降
+    def stocGradDescent(self, featData, labelData, maxCycles):
         dataMat = np.mat(featData)
 	labelMat = np.mat(labelData)
         m, n = np.shape(dataMat)
@@ -41,11 +32,12 @@ class LogisticRegression():
         for i in range(maxCycles):
 	    for i in range(m):
 	        alpha = 4 / (1.0 + j + i) + 0.01
-		randIndex = int(np.random.uniform(0, len(dataIndex)))
+		randIndex = int(np.random.uniform(0, len(dataIndex))) # 随机选择一部分样本进行更新
 		h = self.sigmoid(dataMat[randIndex] * weights)
 		weights = weights + alpha * error * dataMat[randIndex]
 		del(dataIndex[randIndex])
 	return weights
+
     def fit(self, train_x, train_y, alpha=0.01, maxCycles=100):
         return self._gradDescent(train_x, train_y, alpha, maxCycles)
 
